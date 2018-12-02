6da4e9e9232a8be948f91e56e4080c4a5fd61898
Abhay Kshatriya <kshatriya.abhay@iitg.ac.in>
2018-12-03 05:07:11 +0530

    upload ArrowExperience v1.5
        * Credits go to http://t.me/san1tyy (also known as JebaitedXD)
        * This is essentially a continuation of his project.
    
    Signed-off-by: Abhay Kshatriya <kshatriya.abhay@iitg.ac.in>

--- config.sh
+++ config.sh
@@ -41,7 +41,7 @@ LATESTARTSERVICE=false
 
 print_modname() {
   ui_print "*******************************"
-  ui_print "     Magisk Module Template    "
+  ui_print "ArrowExperience1.5"
   ui_print "*******************************"
 }
 
